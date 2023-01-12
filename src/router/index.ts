5275d1f2db0b7aa9bda19669f6f3240824503649
Allan Jeremy <aj.dev254@gmail.com>
2023-01-12 19:11:40 +0300

    build: added training files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "Home",
+      component: import("/components/2.views/About.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/about",
+      name: "About",
+      component: About,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      beforeEnter(to, from, next) {
+        const loggedIn = localStorage.getItem("vue-training-username");
+        if (loggedIn) {
+          next({ path: "/" });
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+      beforeEnter(to, from, next) {
+        const loggedIn = localStorage.getItem("vue-training-username");
+        if (!loggedIn) {
+          next({ path: "/login" });
+        } else {
+          next();
+        }
+      },
+      children: [
+        {
+          path: "",
+          name: "Default",
+          component: Default,
+        },
+        {
+          path: "article/:article_id",
+          name: "Article",
+          component: Article,
+        },
+        {
+          path: "create",
+          name: "Create",
+          component: Create,
+        },
+      ],
+    },
+    {
+      path: "/exercise/1",
+      component: ExerciseOne,
+    },
+    {
+      path: "/exercise/2",
+      component: ExerciseTwo,
+    },
+  ],
+});
 
-export default router
+export default router;
