67b87269b425f641337aff16b93fb8306bff206c
Allan Jeremy <aj.dev254@gmail.com>
2023-01-13 12:33:58 +0300

    build: enabled dashboard related routes - need to fix broken styling

--- src/router/index.js
+++ src/router/index.js
@@ -13,48 +13,48 @@ const router = createRouter({
       name: 'About',
       component: import('@/components/2.views/About.vue'),
     },
-    // {
-    //   path: "/login",
-    //   name: "Login",
-    //   component: Login,
-    //   beforeEnter(to, from, next) {
-    //     const loggedIn = localStorage.getItem("vue-training-username");
-    //     if (loggedIn) {
-    //       next({ path: "/" });
-    //     } else {
-    //       next();
-    //     }
-    //   },
-    // },
-    // {
-    //   path: "/dashboard",
-    //   component: Dashboard,
-    //   beforeEnter(to, from, next) {
-    //     const loggedIn = localStorage.getItem("vue-training-username");
-    //     if (!loggedIn) {
-    //       next({ path: "/login" });
-    //     } else {
-    //       next();
-    //     }
-    //   },
-    //   children: [
-    //     {
-    //       path: "",
-    //       name: "Default",
-    //       component: Default,
-    //     },
-    //     {
-    //       path: "article/:article_id",
-    //       name: "Article",
-    //       component: Article,
-    //     },
-    //     {
-    //       path: "create",
-    //       name: "Create",
-    //       component: Create,
-    //     },
-    //   ],
-    // },
+    {
+      path: '/login',
+      name: 'Login',
+      component: import('@/components/2.views/Login.vue'),
+      beforeEnter(to, from, next) {
+        const loggedIn = localStorage.getItem('vue-training-username');
+        if (loggedIn) {
+          next({ path: '/' });
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: '/dashboard',
+      component: import('@/components/2.views/Dashboard.vue'),
+      beforeEnter(to, from, next) {
+        const loggedIn = localStorage.getItem('vue-training-username');
+        if (!loggedIn) {
+          next({ path: '/login' });
+        } else {
+          next();
+        }
+      },
+      children: [
+        {
+          path: '',
+          name: 'Default',
+          component: import('@/components/3.sections/dashboard/Default.vue'),
+        },
+        {
+          path: 'article/:article_id',
+          name: 'Article',
+          component: import('@/components/3.sections/dashboard/Article.vue'),
+        },
+        {
+          path: 'create',
+          name: 'Create',
+          component: import('@/components/3.sections/dashboard/Create.vue'),
+        },
+      ],
+    },
     // {
     //   path: "/exercise/1",
     //   component: ExerciseOne,
