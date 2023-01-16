0ca79b96db46d694976f5eb6e931f4a895471919
Allan Jeremy <aj.dev254@gmail.com>
2023-01-16 12:16:35 +0300

    build: ported exerciseFour solution to Pinia

--- src/stores/solutions/exerciseFour.js
+++ src/stores/solutions/exerciseFour.js
@@ -1,27 +1,21 @@
-import { getJoke, getJokes } from './../../assets/jokes.js';
+import { defineStore } from 'pinia';
+import { getJoke, getJokes } from '../../assets/jokes';
 
-export default {
+export const useJokeStore = defineStore('joke', {
   state: {
     jokes: [],
   },
   getters: {
     getJoke: (state) => (id) => state.jokes.find((joke) => joke.id === id), //to use a getter, we need to make sure that jokes are always retrieved on load!
   },
-  mutations: {
-    SET_JOKES(state, jokes) {
-      state.jokes = jokes;
-    },
-  },
   actions: {
-    async getJokes({ commit }) {
+    async getJokes() {
       const jokes = await getJokes();
-      commit('SET_JOKES', jokes);
+      this.jokes = jokes;
     },
-    // eslint-disable-next-line no-unused-vars
-    async getJoke({ commit }, joke_id) {
-      // eslint-disable-line no-unused-vars
+    async getJoke(joke_id) {
       const joke = await getJoke(joke_id); //we can use this approach in the case in which we do not want to retrieve the full list on load
       return joke;
     },
   },
-};
+});
