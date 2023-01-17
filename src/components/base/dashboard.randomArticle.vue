e5e960d61f6a7616ef851e838c0f80e4809a3ced
Allan Jeremy <aj.dev254@gmail.com>
2023-01-17 17:57:49 +0300

    build: added module 2 files

---
+++ src/components/base/dashboard.randomArticle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p class="title">{{ title }}</p>
+    <p class="content">{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RandomArticle',
+  props: {
+    article_id: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  text-transform: capitalize;
+  font-weight: bold;
+}
+
+.content {
+  padding-top: 10px;
+}
+</style>
--- src/components/solutions/Default_solution.vue
+++ src/components/solutions/Default_solution.vue
@@ -19,41 +19,31 @@
       </li>
     </ol>
     <p v-else class="default__no-data">No data matching your filter!</p>
+    <div v-if="randomArticle">
+      <h3>Random Article</h3>
+      <RandomArticle
+        class="default__random-article"
+        :article_id="randomArticle.id"
+        :title="randomArticle.title"
+        :content="randomArticle.content"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import RandomArticle from '@/components/base/dashboard.randomArticle';
+import getArticles from '@/assets/articles.js';
+
 export default {
   name: 'Default',
+  components: {
+    RandomArticle,
+  },
   data() {
     return {
       filter: '',
-      articles: [
-        {
-          id: 1,
-          content:
-            'Accessibility is the practice of making your websites usable by as many people as possible. We traditionally think of this as being about people with disabilities, but the practice of making sites accessible also benefits other groups such as those using mobile devices, or those with slow network connections.\n\nYou might also think of accessibility as treating everyone the same, and giving them equal opportunities, no matter what their ability or circumstances. Just as it is wrong to exclude someone from a physical building because they are in a wheelchair (modern public buildings generally have wheelchair ramps or elevators), it is also not right to exclude someone from a website because they have a visual impairment. We are all different, but we are all human, and therefore have the same human rights.',
-          title: 'What is accessibility?',
-        },
-        {
-          id: 2,
-          content:
-            'CSS stands for Cascading Style Sheets with an emphasis placed on “Style.” While HTML is used to structure a web document (defining things like headlines and paragraphs, and allowing you to embed images, video, and other media), CSS comes through and specifies your document’s style—page layouts, colors, and fonts are all determined with CSS. Think of HTML as the foundation (every house has one), and CSS as the aesthetic choices (there’s a big difference between a Victorian mansion and a mid-century modern home).',
-          title: 'What is CSS?',
-        },
-        {
-          id: 3,
-          content:
-            'Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.',
-          title: 'What is Vue.js?',
-        },
-        {
-          id: 4,
-          content:
-            'JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area.',
-          title: 'What is JavaScript?',
-        },
-      ],
+      articles: [],
     };
   },
   computed: {
@@ -66,6 +56,11 @@ export default {
         return content.includes(filter) || title.includes(filter);
       });
     },
+    randomArticle() {
+      if (this.articles.length === 0) return;
+      const randomArticleId = this.getRandomArticleId(this.articles.length);
+      return this.articles.find((article) => article.id === randomArticleId);
+    },
   },
   methods: {
     getArticleDetails(article) {
@@ -73,6 +68,12 @@ export default {
 
       return `${article.content.substring(0, 160)} ...`;
     },
+    getRandomArticleId(length) {
+      return Math.floor(Math.random() * length) + 1;
+    },
+  },
+  async created() {
+    this.articles = await getArticles();
   },
 };
 </script>
