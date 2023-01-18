a5f5a61c7af668b755150204f98f506381803e39
Allan Jeremy <aj.dev254@gmail.com>
2023-01-18 12:05:31 +0300

    chore: remove mixin part of exercise

--- src/components/ExerciseTwo.vue
+++ src/components/ExerciseTwo.vue
@@ -14,7 +14,6 @@
 // - use of multiple vue directives
 // - async component loading
 // - use of properties
-// - use of mixins
 
 //Steps
 // - async import our ExcerciseOne component
@@ -24,12 +23,6 @@
 // - Pass the properties down from the current component to "excerciseOne"
 // - Change ExerciseOne to accept properties instead than load its own data (youneed to leave the property used for the hide and show of the punch line)
 // - Create a "loading" HTML and add v-if to make sure there is no error while data is loaded
-
-//EXTRA
-// - Load the Log mixins in the current component
-// - change Excercise one to "emit" a msg when button is clicked
-// - log something (from the parent component) using the Mixins when the button is clicked in the child component
-// - change the colour of the log msg
 
 // import { fetchJokes } from '@/assets/jokes.js';
 </script>
