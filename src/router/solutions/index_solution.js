c370aaa1a43da4acba3d12d464bb6ab3f9736c95
Allan Jeremy <aj.dev254@gmail.com>
2023-01-19 14:47:42 +0300

    fix: fix issue with routes

--- src/router/solutions/index_solution.js
+++ src/router/solutions/index_solution.js
@@ -1,64 +1,20 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '@/components/2.views/Home.vue';
-import Dashboard from '@/components/2.views/Dashboard.vue';
-
-import ExerciseOne from '@/components/2.views/ExerciseOne.vue';
-import ExerciseTwo from '@/components/2.views/ExerciseTwo.vue';
-
-const About = () =>
-  import(/* webpackChunkName: 'about' */ '@/components/2.views/About.vue');
-const Article = () =>
-  import(
-    /* webpackChunkName: 'article' */ '@/components/3.sections/dashboard/Article.vue'
-  );
-const Default = () =>
-  import(
-    /* webpackChunkName: 'default' */ '@/components/3.sections/dashboard/Default.vue'
-  );
-
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
-  {
-    path: '/dashboard',
-    component: Dashboard,
-    children: [
-      {
-        path: '',
-        name: 'Default',
-        component: Default,
-      },
-      {
-        path: 'article/:article_id',
-        name: 'Article',
-        component: Article,
-      },
-    ],
-  },
-  {
-    path: '/exercise/1',
-    component: ExerciseOne,
-  },
-  {
-    path: '/exercise/2',
-    component: ExerciseTwo,
-  },
-];
-
-const router = new VueRouter({
-  routes,
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/dashboard',
+      component: () => import('@/components/2.views/Dashboard.vue'),
+      children: [
+        {
+          path: '/',
+          component: () =>
+            import('@/components/3.sections/dashboard/Default.vue'),
+        },
+      ],
+    },
+  ],
 });
 
 export default router;
