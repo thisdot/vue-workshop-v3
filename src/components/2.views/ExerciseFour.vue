ed476fe63ae7ea4e33211a8173e3f3ff7c49a572
Allan Jeremy <aj.dev254@gmail.com>
2023-01-26 09:19:09 +0300

    chore: update exercises

--- src/components/2.views/ExerciseFour.vue
+++ src/components/2.views/ExerciseFour.vue
@@ -4,26 +4,26 @@
 
 <script>
 // #Exercise 4
-// Summary: This exercise is aimed at introducing you to vuex
-// Story: An an user I would like to have all my jokes being handled and stored in my vuex store.
+// Summary: This exercise is aimed at introducing you to Pinia
+// Story: An an user I would like to have all my jokes being handled and stored in my Pinia store.
 //        I would also like to have the ability to "add" new jokes.
 
 // This exercise will feature:
-// - Vuex module
-// - use of getter / mutation / action and state
-// - Vuex Integration in the component
+// - Pinia module
+// - use of getter / action and state
+// - Pinia Integration in the component
 
 //Steps
-// - Create a new file within the store/modules folder
-// - Load the new module within the store/index.js
-// - Create an action / mutation / state to fetch the jokes
+// - Create a new file within the stores folder
+// - Load the new module and re-export within the stores/index.js
+// - Create an action / state to fetch the jokes
 // - Change the ExerciseTwo to trigger the action
-// - Change exerciseTwo to load jokes from the store
+// - Change exerciseTwo to load jokes from the stores
 // - Use this file to create a simple component to "ADD jokes"
-// - Call required Vuex Action to "add" joke to the state
+// - Call required Pinia Action to "add" joke to the state
 
 //EXTRA
-// - Use v-model with vuex to add a new joke
+// - Use v-model with Pinia to add a new joke
 // - Add a watched in the ExerciseTwo to trigger a "log" when Jokes changes
 // - Change the values in the chrome/firefox debugger
 </script>
