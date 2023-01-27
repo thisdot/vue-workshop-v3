e871e5f981abf38348653c36a02d994bc4a38add
Allan Jeremy <aj.dev254@gmail.com>
2023-01-27 10:30:40 +0300

    chore: merged exercise 4 into 5

---
+++ src/components/2.views/ExerciseFive.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// #Exercise 5
+// Summary: This exercise is aimed at introducing you to Pinia
+// Story: An an user I would like to have all my jokes being handled and stored in my Pinia store.
+//        I would also like to have the ability to "add" new jokes.
+
+// This exercise will feature:
+// - Pinia module
+// - use of getter / action and state
+// - Pinia Integration in the component
+
+//Steps
+// - Create a new file within the stores folder
+// - Load the new module and re-export within the stores/index.js
+// - Create an action / state to fetch the jokes
+// - Change the ExerciseTwo to trigger the action
+// - Change exerciseTwo to load jokes from the stores
+// - Use this file to create a simple component to "ADD jokes"
+// - Call required Pinia Action to "add" joke to the state
+
+//EXTRA
+// - Use v-model with Pinia to add a new joke
+// - Add a watched in the ExerciseTwo to trigger a "log" when Jokes changes
+// - Change the values in the chrome/firefox debugger
+</script>
+
+<style></style>
--- src/components/3.sections/dashboard/Default.vue
+++ src/components/3.sections/dashboard/Default.vue
@@ -36,9 +36,8 @@
 </template>
 
 <script>
-import { mapGetters, mapState } from 'pinia';
-import { useArticleStore } from '@/stores';
 import RandomArticle from '@/components/4.base/dashboard.randomArticle.vue';
+import getArticles from '@/assets/articles.js';
 
 export default {
   name: 'Default',
@@ -48,10 +47,10 @@ export default {
   data() {
     return {
       filter: '',
+      articles: [],
     };
   },
   computed: {
-    ...mapState(useArticleStore, ['articles']),
     filteredArticles() {
       return this.articles.filter((article) => {
         const content = article.content.toLowerCase();
@@ -61,7 +60,11 @@ export default {
         return content.includes(filter) || title.includes(filter);
       });
     },
-    ...mapGetters(useArticleStore, ['randomArticle']),
+    randomArticle() {
+      if (this.articles.length === 0) return;
+      const randomArticleId = this.getRandomArticleId(this.articles.length);
+      return this.articles.find((article) => article.id === randomArticleId);
+    },
   },
   methods: {
     getArticleDetails(article) {
@@ -69,6 +72,12 @@ export default {
 
       return `${article.content.substring(0, 160)} ...`;
     },
+    getRandomArticleId(length) {
+      return Math.floor(Math.random() * length) + 1;
+    },
+  },
+  async created() {
+    this.articles = await getArticles();
   },
 };
 </script>
