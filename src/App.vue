027081b2c9f77eeffe9bee6715becc8b382be173
Allan Jeremy <allan.jeremy@thisdot.co>
2023-04-19 15:39:04 +0300

    build: remove purple background

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,4 @@
   </div>
 </template>
 
-<style lang="scss">
-body {
-  background: rgb(92, 109, 231);
-  background: linear-gradient(
-    45deg,
-    rgba(92, 109, 231, 1) 0%,
-    rgba(121, 94, 233, 1) 100%
-  );
-}
-</style>
+
