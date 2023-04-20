0c138b894cb44a7ba8672fc24fb4577d5d9533a9
Elanor Riley <oranges13@users.noreply.github.com>
2023-04-20 12:24:50 -0400

    Fix Typo in ExerciseThree

--- src/components/2.views/ExerciseThree.vue
+++ src/components/2.views/ExerciseThree.vue
@@ -21,7 +21,7 @@ import { fetchJokes } from '@/assets/jokes.js';
 import ExerciseOne from './ExerciseOne.vue';
 
 export default {
-  name: 'ExerciseSix',
+  name: 'ExerciseThree',
   components: {
     ExerciseOne,
   },
